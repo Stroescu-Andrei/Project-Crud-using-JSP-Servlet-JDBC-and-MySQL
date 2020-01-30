d5405b167f1fdbb20a4b54cf2df6d62d17b0b62f
Stroescu-Andrei <37418903+Stroescu-Andrei@users.noreply.github.com>
2020-01-31 00:13:09 +0200

    Java Web Dynamic

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Project-Crud-using-JSP-Servlet-JDBC-and-MySQL
-Java Web 
+# todo-application-jsp-servlet-jdbc-mysql
+
+Refer blog tutorial at 
+<a href="https://www.javaguides.net/2019/10/build-todo-app-using-jsp-servlet-jdbc-and-mysql.html"> Build Todo App using JSP, Servlet, JDBC and MySQL</a>
---
+++ src/daw/model/dao/TodoDao.java
@@ -0,0 +1,20 @@
+package daw.model.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import daw.model.model.Todo;
+
+public interface TodoDao {
+
+	void insertTodo(Todo todo) throws SQLException;
+
+	Todo selectTodo(long todoId);
+
+	List<Todo> selectAllTodos();
+
+	boolean deleteTodo(int id) throws SQLException;
+
+	boolean updateTodo(Todo todo) throws SQLException;
+
+}
